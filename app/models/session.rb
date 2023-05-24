f1368664533bdb7917cab0dce02a688b38223bf5
Nay Myo Lwin <naymyolwin@gmail.com>
2023-05-24 23:27:05 +0700

    add session validation and association

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,9 @@
 class Session < ApplicationRecord
+    validates :user_id, presence: true
+    before_validation :generate_session_token
+    belongs_to :user
+
+    def generate_session_token
+        self.token = SecureRandom.urlsafe_base64
+    end
 end
