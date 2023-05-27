acc0070e6d34b387bc9216669088c4a7ab26480c
Nay Myo Lwin <naymyolwin@gmail.com>
2023-05-27 19:39:22 +0700

    add methods for login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
 
     def create
-        @user = User.new
-    end
+        @user = User.new(user_params)
+        if @user.save
+          render json: {
+            user: {
+              username: @user.username
+            }
+          }
+        else
+          render json: {
+            success: false
+          }
+        end
+      end
+    
+      private
+        def user_params
+          params.require(:user).permit(:username, :email, :password)
+        end
 
 end
